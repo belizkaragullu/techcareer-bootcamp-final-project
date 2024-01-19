378e15765d9c591f9182d5c6128a6f2755cb6b17
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-19 22:31:05 +0300

    get all posts controller added

--- blog-project/src/main/java/com/example/blogproject/controller/PostController.java
+++ blog-project/src/main/java/com/example/blogproject/controller/PostController.java
@@ -5,10 +5,9 @@ import com.example.blogproject.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +20,10 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get")
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPosts();
     }
 }
