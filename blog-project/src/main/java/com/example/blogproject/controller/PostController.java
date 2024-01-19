810dd08c66b7c2bc5cc662fce5c8ffa573809f50
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-19 22:03:07 +0300

    post controller added

---
+++ blog-project/src/main/java/com/example/blogproject/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.example.blogproject.controller;
+
+import com.example.blogproject.payload.PostDto;
+import com.example.blogproject.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+    //interface to maintain loosecoupling
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
