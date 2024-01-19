5c9c77956f42b3f390182f435176bef7b2c95541
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-19 19:11:10 +0300

    postdto added

--- blog-project/src/main/java/com/example/blogproject/payload/PostDto.java
+++ blog-project/src/main/java/com/example/blogproject/payload/PostDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class PostDto {
     private Long id;
-    private String field;
     private String description;
     private String content;
+    private String title;
 }
