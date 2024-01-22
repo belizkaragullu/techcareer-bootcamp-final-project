e89cee548443b71b1a8ff55caec6332c2ac4dc34
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-22 18:14:47 +0300

    getCommentByPostId added

--- blog-project/src/main/java/com/example/blogproject/controller/CommentController.java
+++ blog-project/src/main/java/com/example/blogproject/controller/CommentController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/")
@@ -21,5 +23,9 @@ public class CommentController {
         return new ResponseEntity<>(commentService.createComment(postId,commentDto), HttpStatus.CREATED);
     }
 
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable(value= "postId") Long postId){
+        return new ResponseEntity<>(commentService.getCommentsByPostId(postId), HttpStatus.OK);
+    }
 
 }
--- blog-project/src/main/java/com/example/blogproject/service/CommentService.java
+++ blog-project/src/main/java/com/example/blogproject/service/CommentService.java
@@ -2,7 +2,10 @@ package com.example.blogproject.service;
 
 import com.example.blogproject.payload.CommentDto;
 
+import java.util.List;
+
 public interface CommentService {
     CommentDto createComment(Long postId, CommentDto commentDto);
+    List<CommentDto> getCommentsByPostId(Long postId);
 
 }
