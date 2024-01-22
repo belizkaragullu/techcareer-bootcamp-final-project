6eb66c750cb21ac212268e2be76ec6e7f5ed2a11
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-22 15:58:13 +0300

    comment dto mapper added

--- blog-project/src/main/java/com/example/blogproject/service/impl/CommentServiceImpl.java
+++ blog-project/src/main/java/com/example/blogproject/service/impl/CommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.blogproject.service.impl;
 
+import com.example.blogproject.entity.Comment;
 import com.example.blogproject.payload.CommentDto;
 import com.example.blogproject.repository.CommentRepository;
 import com.example.blogproject.service.CommentService;
@@ -12,5 +13,26 @@ public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
 
+    private CommentDto mapToDto(Comment comment){
+        CommentDto commentDto = new CommentDto();
+
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setText(commentDto.getText());
+        commentDto.setEmail(comment.getEmail());
+
+        return commentDto;
+    }
+
+    private Comment mapToEntity(CommentDto commentDto){
+        Comment comment = new Comment();
+
+        comment.setId(commentDto.getId());
+        comment.setEmail(commentDto.getEmail());
+        comment.setText(commentDto.getText());
+        comment.setName(commentDto.getName());
+
+        return comment;
+    }
 
 }
