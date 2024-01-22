a132cc9d5b3575ac3577bfc7f7765e724885dbda
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-22 16:25:20 +0300

    createComment controller

---
+++ blog-project/src/main/java/com/example/blogproject/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.example.blogproject.controller;
+
+import com.example.blogproject.payload.CommentDto;
+import com.example.blogproject.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value= "postId") Long postId,
+                                                    @RequestBody CommentDto commentDto){
+
+        return new ResponseEntity<>(commentService.createComment(postId,commentDto), HttpStatus.OK);
+    }
+
+
+}
