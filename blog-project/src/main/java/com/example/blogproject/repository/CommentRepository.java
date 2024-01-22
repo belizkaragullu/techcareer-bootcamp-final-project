c1c64f4ee02953da519e9c104498843dd47f93be
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-22 18:14:12 +0300

    customized jpa

--- blog-project/src/main/java/com/example/blogproject/repository/CommentRepository.java
+++ blog-project/src/main/java/com/example/blogproject/repository/CommentRepository.java
@@ -3,6 +3,9 @@ package com.example.blogproject.repository;
 import com.example.blogproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 //no need to specify repository annotation
+    List<Comment> findByPostId(Long id);
 }
