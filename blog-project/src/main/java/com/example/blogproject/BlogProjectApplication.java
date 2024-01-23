5afcabf701f37ed45658062549437ea6147cd512
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-23 16:30:01 +0300

    modelMapper bean defined

--- blog-project/src/main/java/com/example/blogproject/BlogProjectApplication.java
+++ blog-project/src/main/java/com/example/blogproject/BlogProjectApplication.java
@@ -1,10 +1,17 @@
 package com.example.blogproject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogProjectApplication {
+
+	@Bean //Define modelmapper bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogProjectApplication.class, args);
