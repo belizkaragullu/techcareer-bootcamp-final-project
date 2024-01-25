e652bc20cc5ddcdfbfaf5daf4bb96c33409c73d6
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-25 23:15:19 +0300

    user and role entities created for db auth

---
+++ blog-project/src/main/java/com/example/blogproject/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.blogproject.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch= FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name ="user_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+
+}
