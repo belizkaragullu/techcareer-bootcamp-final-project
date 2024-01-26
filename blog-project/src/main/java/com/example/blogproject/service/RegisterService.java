18bb41641cacf62c7c25190a1b6614fd0aa2c521
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-26 12:15:14 +0300

    register service implemented

---
+++ blog-project/src/main/java/com/example/blogproject/service/RegisterService.java
@@ -0,0 +1,7 @@
+package com.example.blogproject.service;
+
+import com.example.blogproject.payload.RegisterDto;
+
+public interface RegisterService {
+    String register(RegisterDto registerDto);
+}
