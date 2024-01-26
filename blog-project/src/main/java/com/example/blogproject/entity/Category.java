a6e6437420b7a58974ee0899b25a0f4c9b7891ba
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-26 12:40:55 +0300

    category entity added

--- blog-project/src/main/java/com/example/blogproject/entity/Category.java
+++ blog-project/src/main/java/com/example/blogproject/entity/Category.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,5 +20,8 @@ public class Category {
     private Long id;
     @Column(nullable = false,unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
 
 }
