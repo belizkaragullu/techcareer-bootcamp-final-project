81d7fb4f5dbcdd3c9eb8ddb643c615d876cab823
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-26 12:14:26 +0300

    new constructor added to blogexception

--- blog-project/src/main/java/com/example/blogproject/exception/BlogException.java
+++ blog-project/src/main/java/com/example/blogproject/exception/BlogException.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BlogException extends RuntimeException{
 
+    private HttpStatus status;
+    private String message;
 
     public BlogException(String message) {
        super(message);
+    }
+    public BlogException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
     }
 }
