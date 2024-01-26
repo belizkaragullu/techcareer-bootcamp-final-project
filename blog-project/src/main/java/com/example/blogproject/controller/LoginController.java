3857abf29a8f313c8e902125e42edd5c78beb130
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-26 12:14:46 +0300

    login controller added

---
+++ blog-project/src/main/java/com/example/blogproject/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.example.blogproject.controller;
+
+import com.example.blogproject.payload.LoginDto;
+import com.example.blogproject.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController("api/auth")
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(LoginDto loginDto){
+        String response = loginService.login(loginDto);
+        return ResponseEntity.ok(response);
+    }
+}
