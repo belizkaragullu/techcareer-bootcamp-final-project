cfefa0c10439508ca1ffb9fe7a7d0abd02f9903c
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-27 18:15:24 +0300

    add Set<CommentDto> to PostDto

--- blog-project/src/main/java/com/example/blogproject/payload/PostDto.java
+++ blog-project/src/main/java/com/example/blogproject/payload/PostDto.java
@@ -2,10 +2,13 @@ package com.example.blogproject.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private Long id;
     private String description;
     private String content;
     private String title;
+    private Set<CommentDto> commentDtoSet;
 }
