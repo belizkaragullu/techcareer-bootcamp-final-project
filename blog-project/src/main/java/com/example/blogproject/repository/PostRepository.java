48923389272dc37fb5533db69b8d03bbf6c9963f
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-28 01:55:14 +0300

    postrepository customized method

--- blog-project/src/main/java/com/example/blogproject/repository/PostRepository.java
+++ blog-project/src/main/java/com/example/blogproject/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.example.blogproject.repository;
 import com.example.blogproject.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByCategoryId(Long categoryId);
+
 }
