490f0c320b9b0145ca970aba847fe9ad67b5a3da
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-28 10:35:44 +0300

    no usages erase

--- blog-project/src/main/java/com/example/blogproject/exception/BlogException.java
+++ blog-project/src/main/java/com/example/blogproject/exception/BlogException.java
@@ -6,14 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BlogException extends RuntimeException{
 
-    private HttpStatus status;
-    private String message;
-
     public BlogException(String message) {
        super(message);
     }
-    public BlogException(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+
 }
